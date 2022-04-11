53657010421c4741c37d97f13a95bac26b3251d8
Don616 <contatodonilo@gmail.com>
2022-04-10 23:09:32 -0300

    telas de cadastro criadas

--- Site/user.js
+++ Site/user.js
@@ -1,3 +1,49 @@
+mudarTela()
+
 function mvDashboard(){
     window.location = "./dashboard/dashboard.html"
+}
+
+function mudarTela(){
+
+    var botao = document.getElementsByClassName('titulo_sidebar')
+
+    for(let i = 0; i < botao.length;i++){
+
+        botao[i].addEventListener('click',()=>{
+
+            let clicado = event.target.innerHTML
+
+            if(clicado == "Minha Conta"){
+                tela_conta.style.display = 'flex'
+            } else {
+                tela_conta.style.display = 'none'
+            }
+            if(clicado == "Totens"){
+                tela_totens.style.display = 'flex'
+            } else {
+                tela_totens.style.display = 'none'
+            }
+            if(clicado == "Colaboradores"){
+                tela_colaboradores.style.display = 'flex'
+            } else {
+                tela_colaboradores.style.display = 'none'
+            }
+            if(clicado == "Regiões"){
+                tela_regioes.style.display = 'flex'
+            } else {
+                tela_regioes.style.display = 'none'
+            }
+            if(clicado == "Mapa"){
+                tela_mapa.style.display = 'flex'
+            } else {
+                tela_mapa.style.display = 'none'
+            }
+
+
+        })
+    }
+
+    
+
 }
