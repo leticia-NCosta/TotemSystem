af9ce59aecaf0b16b95a9f50c6d65b13d016c253
juliadiasbatista <JULIA.BATISTA@bandtec.com.br>
2022-04-21 23:50:17 -0300

    Adicionando dados estáticos

--- Java/totemSistem/src/main/java/br/com/sptech/totemsistem/SalvarDados.java
+++ Java/totemSistem/src/main/java/br/com/sptech/totemsistem/SalvarDados.java
@@ -11,32 +11,44 @@ public class SalvarDados {
     Looca looca = new Looca();
     JdbcTemplate template = new JdbcTemplate(config.getDataSource());
 
-    public void salvarDadosEstaticos(){
+    public void salvarDadosEstaticos() {
 
         String nomeTabela = "tb_totem";
         String sistemaOperacional = looca.getSistema().getSistemaOperacional();
+    //    String Hostname = looca.getSistema().getHostname();
+        String Fabricante = looca.getSistema().getFabricante();
+        Integer Arquitetura = looca.getSistema().getArquitetura();
+        Instant InicializadoEm = looca.getSistema().getInicializado();
+        Double Permissões = looca.getSistema().getPermissao();
+//        
+        String Marca = looca.getProcessador().getNome();
+        String Fabricante = looca.getProcessador().getFabricante();
+        String MicroArquitetura =looca.getProcessador().getMicroarquitetura();
+        String CPUsFisicas =looca.getProcessador().getNumeroCpusFisicas();
+        String CPUsLogicas = looca.getProcessador().getNumeroCpusLogicas();
+        String PacotesFisicos = looca.getProcessador().getNumeroPacotesFisicos();
+        String Frequencia =looca.getProcessador().getFrequencia();
 
         String inserirDado = String.format("INSERT INTO %s"
-        + "(sistemaOperacional) VALUES"
-        + "('%s')",nomeTabela,sistemaOperacional);
+                + "(sistemaOperacional,Hostname,Fabricante,Arquitetura,InicializadoEm,Permissões,"
+                + "Marca,Fabricante, Micro arquitetura,CPUs Físicas,CPUs Lógicas,Pacotes Físicos,Frequência) VALUES"
+                + "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')", nomeTabela, sistemaOperacional);
 
         template.execute(inserirDado);
 
     }
 
-    public void salvarDadosVariaveis(){
+    public void salvarDadosVariaveis() {
 
         String nomeTabela = "tb_registro";
         String sistemaOperacional = looca.getSistema().getSistemaOperacional();
 
         String inserirDado = String.format("INSERT INTO %s"
                 + "(sistemaOperacional) VALUES"
-                + "('%s')",nomeTabela,sistemaOperacional);
+                + "('%s')", nomeTabela, sistemaOperacional);
 
         template.execute(inserirDado);
 
     }
 
 }
-
-
