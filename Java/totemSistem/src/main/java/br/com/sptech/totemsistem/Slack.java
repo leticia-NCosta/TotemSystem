f28519e800732c88a349edad2be1880c95e7d564
unknown <leticia.costa@bandtec.com.br>
2022-05-03 21:18:22 -0300

    Alteração de mapa e a integração do slack no java

---
+++ Java/totemSistem/src/main/java/br/com/sptech/totemsistem/Slack.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sptech.totemsistem;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Letícia
+ */
+public class Slack {
+    private static HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+    
+    
+    public static void sendMessage(JSONObject content)throws IOException, InterruptedException{ 
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(URL))
+                .header("accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                .build();
+        
+        HttpResponse<String> response =  client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        System.out.println(String.format("Status: %s", response.statusCode()));
+        System.out.println(String.format("Response: %s", response.body()));
+    }
+}
