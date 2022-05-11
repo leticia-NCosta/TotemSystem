f4b1cfe2490b19a84ec65e2102a7578ad342c070
JPedroDeCarvalho <JOAO.CARVALHO@bandtec.com.br>
2022-05-10 21:33:08 -0300

    logs

--- Java/totemSistem/src/main/java/br/com/sptech/totemsistem/LogTxt.java
+++ Java/totemSistem/src/main/java/br/com/sptech/totemsistem/LogTxt.java
@@ -13,6 +13,7 @@ import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+//leitor de arquivo
 import java.io.FileReader;
 import java.io.BufferedReader;
 
@@ -21,13 +22,21 @@ public class LogTxt {
 
     public static void main(String[] args) throws IOException {
 
-        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("dd-MM-yy_HH-mm-ss");
         DateTimeFormatter dtf3 = DateTimeFormatter.ofPattern("dd/MM/yy HH-mm-ss");
 
         String arquivo = "Log.txt";
 
         //enquanto houver mais linhas
         try {
+            //Arquivo existe
+
+            String hostname = "";
+            try {
+                hostname = InetAddress.getLocalHost().getHostName();
+            } catch (UnknownHostException existe) {
+                existe.printStackTrace();
+            }
+
             FileReader file = new FileReader("/home/aluno/Desktop/" + arquivo);
             BufferedReader reader = new BufferedReader(file);
             reader.ready();
@@ -35,17 +44,30 @@ public class LogTxt {
             file.close();
             System.out.println("Existe");
 
-            //Arquivo existe
-        } catch (Exception e0) {
+            FileWriter arq = new FileWriter("/home/aluno/Desktop/" + arquivo); // Nessa linha escolhemos a rota onde o arquivo ira ser salvado
+            PrintWriter gravarArq = new PrintWriter(arq);
+            
 
-            // Não existe
+            gravarArq.printf(("\nUsuário: " + hostname));
+            gravarArq.printf(("\nData e Hora: " + dtf3.format(LocalDateTime.now())));
+
+            gravarArq.println("\n \n \n ERRO DE LOGIN");
+
+            arq.append("\nteste");
+            arq.close();
+
+            System.out.println("LOG .txt GERADO EM: /home/aluno/Desktop/");
+
+        } catch (Exception naoExiste) {
+
+            //Arquivo não existe
             System.out.println("Nao existe");
 
             String hostname = "";
             try {
                 hostname = InetAddress.getLocalHost().getHostName();
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
+            } catch (UnknownHostException existe) {
+                existe.printStackTrace();
             }
 
             // String arquivo = String.format("Log-%s.txt", dtf2.format(LocalDateTime.now()));
